001c582182673d8688a2bdd3a1f72054c5d8b9e2
Adam Natan Passaretti <adapassa@c2r3p12.42firenze.it>
2024-08-05 17:45:52 +0200

    removed_old_code

--- src/parser.c
+++ src/parser.c
@@ -6,86 +6,11 @@
 /*   By: adapassa <adapassa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/10 14:04:42 by adapassa          #+#    #+#             */
-/*   Updated: 2024/08/05 17:35:11 by adapassa         ###   ########.fr       */
+/*   Updated: 2024/08/05 17:44:27 by adapassa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minishell.h"
-//echo lyc < ciao | cat -e > merda
-
-// static t_token resolve_quotes(char *str)
-// {
-// 	int	i;
-// 	char *tmp;
-
-// 	i = 0;
-// 	tmp = ft_calloc(, ft_strlen(str - 1));
-// 	// a command will be the first string token
-// 	// not preceded by a special character
-// 	while (i < ft_strlen(str))
-// 	{
-		
-// 	}
-// }
-
-// static void	parse_quote_token(char *str, t_data *data, t_token **tokens)
-// {
-// 	t_token	tmp;
-
-// 	tmp = NULL;
-// 	tmp = resolve_quotes(str, tokens);
-// }
-
-// static	int	quote_check(char *str)
-// {
-// 	int	i;
-
-// 	i = 0;
-// 	// Debug
-// 	//ft_printf("%s\n", str);
-// 	while (i < ft_strlen(str) - 1)
-// 	{
-// 		if (str[i] == DOUBLE_QUOTES)
-// 		{
-// 			return (1);
-// 		}
-// 		i++;
-// 	}
-// 	return (0);
-//  }
-
-// void	token_parser(t_data *data, t_token **tokens, char **envp)
-// {
-// 	t_token *current;
-
-// 	current = *tokens;
-// 	ft_printf("\nStarting Input Parsing: \n--------------------------------------\n");
-// 	while (current->next != NULL)
-// 	{
-// 		if (quote_check(current->value) > 0)
-// 		{
-// 			ft_printf("Found DQUOTES State, Parsing:\n");
-// 			parse_quote_token(current, data, tokens);
-// 		}
-// 		current = current->next;
-// 	}
-// 	ft_printf("\nPrinting token lists: \n--------------------------------------\n");
-// }
-
-// char	*expand_variable(t_token **tokens, char **envp)
-// {
-// 	t_token	*current;
-// 	current = *tokens;
-// 	char	*result;
-// 	ft_printf("expanding the variable\n");
-
-// 	//printf("%s\n", current->value);
-// 	if (current->value[0] == DOLLAR_SIGN)
-// 	{
-// 		ft_printf("found dollar sign!\n");
-// 	}
-// 	return (result);
-// }
 
 char	*find_cmd(char *cmd, t_data *data)
 {
@@ -220,7 +145,6 @@ void	token_parser(t_token **tokens, t_data *data, char **envp)
 				data->redirect_state = 1;
 				if (current->type == TOKEN_APPENDICE)
 				{
-					//printf("ciao cane\n");
 					data->fd = open(current->value, O_CREAT | O_RDWR | O_TRUNC, 0644);
 					printf("%d\n", data->fd);
 				}
@@ -233,7 +157,6 @@ void	token_parser(t_token **tokens, t_data *data, char **envp)
 				data->redirect_state = 0;
 				if (current->type == TOKEN_APPENDICE)
 				{
-					//printf("ciao cane\n");
 					data->fd = open(current->value, O_RDONLY);
 					printf("%d\n", data->fd);
 				}
@@ -266,16 +189,6 @@ void	token_parser(t_token **tokens, t_data *data, char **envp)
 			//current = current->next->next;
 			//continue;
 		}
-		// if (current->type == TOKEN_REDIRECT_IN || current->type == TOKEN_REDIRECT_OUT)
-		// {
-		// 	printf("ititializing redirection\n");
-		// 	command[0] = current->value;
-		// 	command[1] = current->next->value;
-		// 	printf("%s\n", command[0]);
-		// 	printf("%s\n", command[1]);
-		// 	current = current->next->next;
-		// 	continue;
-		// }
 		current = current->next;
 	}
 }
